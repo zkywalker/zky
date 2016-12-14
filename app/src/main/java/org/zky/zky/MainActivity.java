03f0274144a34ec59680cd8ef6c87ea6f2e2a37d
kun <zhan9kun@qq.com>
2016-12-15 00:14:08 +0800

    update drawer's icon and string

--- app/src/main/java/org/zky/zky/MainActivity.java
+++ app/src/main/java/org/zky/zky/MainActivity.java
@@ -80,13 +80,13 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_widget) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_view) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_activity) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_sdk) {
 
         } else if (id == R.id.nav_share) {
 
