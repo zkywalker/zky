ce5f141d92ea4c66cabbfe2552b5cd1abc46f0ca
kun <zhan9kun@qq.com>
2016-12-14 23:37:47 +0800

    a surface view demo

---
+++ app/src/main/java/org/zky/zky/widget/MySurface.java
@@ -0,0 +1,117 @@
+package org.zky.zky.widget;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class MySurface extends SurfaceView implements SurfaceHolder.Callback, Runnable {
+    private static final String TAG = "MySurface";
+    private SurfaceHolder mHolder;
+    private Canvas mCanvas;
+    private boolean mIsDrawing;
+    private Path mPath;
+    private Paint mPaint;
+
+    public MySurface(Context context) {
+        super(context);
+        initView();
+    }
+
+    public MySurface(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initView();
+    }
+
+    public MySurface(Context context, AttributeSet attrs,
+                      int defStyle) {
+        super(context, attrs, defStyle);
+        initView();
+    }
+
+    private void initView() {
+        mHolder = getHolder();
+        mHolder.addCallback(this);
+
+        setFocusable(true);
+        setFocusableInTouchMode(true);
+        this.setKeepScreenOn(true);
+
+        mPath = new Path();
+        mPaint = new Paint();
+        mPaint.setColor(Color.RED);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setAntiAlias(true);
+        mPaint.setStrokeWidth(40);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        mIsDrawing = true;
+        new Thread(this).start();
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder,
+                               int format, int width, int height) {
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        Log.i(TAG, "surfaceDestroyed: ist drawing");
+        mIsDrawing = false;
+    }
+
+    @Override
+    public void run() {
+        long start = System.currentTimeMillis();
+        while (mIsDrawing) {
+            draw();
+        }
+        long end = System.currentTimeMillis();
+        // 50 - 100
+        if (end - start < 100) {
+            try {
+                Thread.sleep(100 - (end - start));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void draw() {
+        try {
+            mCanvas = mHolder.lockCanvas();
+            mCanvas.drawColor(Color.WHITE);
+            mCanvas.drawPath(mPath, mPaint);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (mCanvas != null)
+                mHolder.unlockCanvasAndPost(mCanvas);
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int x = (int) event.getX();
+        int y = (int) event.getY();
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                mPath.moveTo(x, y);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                mPath.lineTo(x, y);
+                break;
+            case MotionEvent.ACTION_UP:
+                break;
+        }
+        return true;
+    }
+}
