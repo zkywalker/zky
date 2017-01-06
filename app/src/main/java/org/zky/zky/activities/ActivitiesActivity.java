1adaa79375d3aa28e98ab1145233424e5f0ea877
keral <zhan9kun@qq.com>
2017-01-06 16:05:33 +0800

    归档

---
+++ app/src/main/java/org/zky/zky/activities/ActivitiesActivity.java
@@ -0,0 +1,15 @@
+package org.zky.zky.activities;
+
+import android.os.Bundle;
+
+import org.zky.zky.BaseThemeActivity;
+import org.zky.zky.R;
+
+public class ActivitiesActivity extends BaseThemeActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activities);
+    }
+}
